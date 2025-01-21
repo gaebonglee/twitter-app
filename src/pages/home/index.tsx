6c31a3473b84e3f379407b6b68162a08c909c38b
gayoung lee <ken604@naver.com>
2025-01-22 02:23:14 +0900

    홈페이지 레이아웃을 위한 테스트 진행

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,40 @@
+export interface PostProps {
+  id: string;
+  email: string;
+  content: string;
+  createAt: string;
+  uid: string;
+}
+const posts: PostProps[] = [
+  {
+    id: "1",
+    email: "test@test.com",
+    content: "ggg",
+    createAt: "2025-01-01",
+    uid: "1231234",
+  },
+  {
+    id: "2",
+    email: "test@test.com",
+    content: "ggg",
+    createAt: "2025-01-01",
+    uid: "1231234",
+  },
+  {
+    id: "3",
+    email: "test@test.com",
+    content: "ggg",
+    createAt: "2025-01-01",
+    uid: "1231234",
+  },
+  {
+    id: "4",
+    email: "test@test.com",
+    content: "ggg",
+    createAt: "2025-01-01",
+    uid: "1231234",
+  },
+];
 export default function Homepage() {
   return <div>homepage</div>;
 }
